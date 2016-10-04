91a4a5782ad7f4fa8c46496adba36260b0c8c8c6
MijianXu <gomijianxu@gmail.com>
2016-10-04 15:51:16 +0800

    init codes

--- uncps.sh
+++ uncps.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+# Unzip seed files by days
+# Author: Mijian Xu @ Nanjing University
+#
+# History: 2016-10-04: Init Codes
+#
+if [ $# == 2 ];then
+    idir=$1
+    ddir=$2
+elif [ $# == 1 ];then
+    idir=$1
+    ddir=./
+else
+    echo "Wrong arguments"
+    exit 1
+fi
 
-idir=$1
-ddir=$2
 if [ ! -d $ddir ];then
     mkdir $ddir
 fi
@@ -12,4 +25,7 @@ do
     odir=`echo $seedfile|awk -F"/" '{split($NF,a,/[_|.]/);print a[2]"."a[3]"."a[4]}'`
     mkdir $ddir/$odir
     rdseed -df -q $seedfile $ddir/$odir
+    rdseed -pf -q $seedfile $ddir/$odir
 done< ttt
+
+rm ttt
